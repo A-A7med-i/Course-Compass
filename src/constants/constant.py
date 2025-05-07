ac142ce2ba5b8defd56a9298b41d3345094a5d83
A7med Nabil <an223602@gmail.com>
2025-05-07 14:42:52 +0300

    Project constant

---
+++ src/constants/constant.py
@@ -0,0 +1,54 @@
+import os
+
+# Application metadata
+PROJECT_NAME = "CourseCompass"
+VERSION = "1.0.0"
+AUTHOR = "Ahmed"
+DESCRIPTION = "An AI agent to help find the best courses."
+
+
+# Model configuration
+MODEL_NAME = "groq/llama-3.3-70b-versatile"
+TEMPERATURE = 0
+
+
+# Paths and file locations
+AGENT_CONFIG_PATH = "/media/ahmed/Data/CourseCompass/config/agent_param.yaml"
+TASK_CONFIG_PATH = "/media/ahmed/Data/CourseCompass/config/task_param.yaml"
+LOG_FILE = "logging.log"
+LOG_DIR = "logs"
+
+
+# Logging configuration
+LOGGING_FORMAT = (
+    "[%(asctime)s] [%(levelname)s] [%(name)s:%(funcName)s:%(lineno)d] - %(message)s"
+)
+DATEFMT = "%Y-%m-%d %H:%M:%S"
+
+
+# API keys and credentials
+GROQ_API_KEY = os.getenv("GROQ_API_KEY")
+TAVILY_API_KEY = os.getenv("TAVILY_API_KEY")
+SCRAPING_API_KEY = os.getenv("SCRAPING_API_KEY")
+
+# Schemas constant
+NO_KEYWORDS = 10
+
+
+# Crew inputs
+INPUTS = {
+    "course_name": "Python Programming",
+    "websites_list": [
+        "Coursera",
+        "Udemy",
+        "edX",
+        "Pluralsight",
+        "LinkedIn Learning",
+    ],
+    "country_name": "United States",
+    "language": "English",
+    "no_keywords": NO_KEYWORDS,
+    "score_th": 0.7,
+    "top_recommendations_no": 5,
+    "company_name": "TechCorp Inc.",
+}
